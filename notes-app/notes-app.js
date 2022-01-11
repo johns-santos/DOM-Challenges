0210559675e556077f64bf6f0cc7684a63f2eb3f
johns-santos <johnsanthony.santos@gmail.com>
2022-01-10 19:17:46 -0600

    adding filter functionality

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -24,3 +24,26 @@ document.querySelector('#remove-all').addEventListener('click', function(e){
 document.querySelector('#search-text').addEventListener('input', function(e){
     console.log(e.target.value)
 })
+
+
+
+const findNote = function(array, string){
+    return notes.filter(function(note, index){
+        const noteTitle = note.title.toLowerCase().includes(string.toLowerCase());
+        const noteBody = note.body.toLowerCase().includes(string.toLowerCase())
+        return noteTitle || noteBody
+
+    })
+}
+
+
+// Search note by title
+// const findNote = function (notes, noteTitle) {
+//     const index = notes.findIndex(function (note, index) {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[index]
+// }
+
+const note = findNote(notes, 'mod');
+console.log(note);
