c0b0504c3ab6c14f3d4ca8d63c9630a16196be93
johns-santos <johnsanthony.santos@gmail.com>
2022-01-13 00:56:46 -0600

    added filter input

--- todos-app/todos-app.js
+++ todos-app/todos-app.js
@@ -1,42 +1,83 @@
-const todos = [{
-    text: "Order dog food",
+const todos = [
+  {
+    text: 'Order dog food',
     completed: false
-},{
-    text: "Fix flat tire",
+  },
+  {
+    text: 'Fix flat tire',
     completed: false
-},
-{
-    text: "Clean living room",
+  },
+  {
+    text: 'Clean living room',
     completed: false
-},
-{
-    text: "Complete application",
+  },
+  {
+    text: 'Complete application',
     completed: true
-}]
+  }
+]
 
-const incomplete = todos.filter(function (todo){
+const filters = {
+  searchText: ''
+}
+
+const renderTodos = function (todos, filters) {
+  const filteredTodos = todos.filter(function (todo) {
+    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+  })
+  // CLEAR .todo-list div after every input
+  document.querySelector('#todo-list').innerHTML = ''
+
+  // Update H2 element which counts todos that are incomplete
+  const incomplete = filteredTodos.filter(function (todo) {
     return !todo.completed
-}) 
+  })
+  const summary = document.createElement('h2')
+  summary.textContent = `You have ${incomplete.length} todos left to complete.`
+  document.querySelector('#todo-list').appendChild(summary)
 
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incomplete.length} todos left to complete.` 
-document.querySelector('#list').appendChild(summary);
+  // Wrap todo in paragraph  element
+  filteredTodos.forEach(function (todo) {
+    const todoElement = document.createElement('p')
+    todoElement.textContent = todo.text
+    document.querySelector('#todo-list').appendChild(todoElement)
+  })
+}
 
-todos.forEach(function (todo){
-    // Add new paragraph element to bottom of list
-    const newParagraph = document.createElement('p');
-    // create content 
-    newParagraph.textContent = todo.text;
-    // specify where to place new element.
-    document.querySelector('.paragraphs').appendChild(newParagraph);  
-})
+renderTodos(todos, filters)
 
 // Add eventListener to button
-document.querySelector('#add-todo').addEventListener('click', function(e){
-    console.log("Button has been clicked.")
+document.querySelector('#add-todo-btn').addEventListener('click', function (e) {
+  console.log('Button has been clicked.')
 })
 
-// Listen for input changes in 
-document.querySelector('#search-text').addEventListener('input', function(e){
-    console.log(e.target.value)
+// Add new-todo input
+document.querySelector('#new-todo').addEventListener('input', function (e) {
+  console.log(e.target.value)
+})
+
+// Listen for input changes in
+document.querySelector('#search-text').addEventListener('input', function (e) {
+  filters.searchText = e.target.value
+  renderTodos(todos, filters)
+})
+
+// SORT todos
+const sortTodos = function (todos) {
+  todos.sort(function (a, b) {
+    if (!a.completed && b.completed) {
+      return -1
+    } else if (!b.completed && a.completed) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+}
+
+// REMOVE TODO Button function
+document.querySelector('#remove-todos').addEventListener('click', function (e) {
+  document.querySelectorAll('p').forEach(function (todo) {
+    todo.remove()
+  })
 })
