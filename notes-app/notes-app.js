77f834216407d28f6296a57e63b2ecc5e6a0b193
johns-santos <johnsanthony.santos@gmail.com>
2022-01-28 03:39:09 -0600

    saving items to localstorage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,17 +1,18 @@
-const notes = [{
-    title: 'My next trip',
-    body: 'I would like to go to Spain'
-},{
-    title: 'Habbits to work on',
-    body: 'Exercise. Eating a bit better'
-},{
-    title: 'Office modification',
-    body: 'Get a new seat'
-}]
+let notes = []
 
 const filters = {
     searchText: ''
 }
+
+// Check for existing saved data
+const notesJSON = localStorage.getItem('notes')
+
+if(notesJSON !== null) {
+    notes = JSON.parse(notesJSON)
+}
+
+
+
 
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function(note){
@@ -22,7 +23,11 @@ const renderNotes = function (notes, filters) {
     // Wrap note in paragraph element 
     filteredNotes.forEach(function (note){
         const noteElement = document.createElement('p')
-        noteElement.textContent = note.title
+        if(note.title.length > 0) {
+            noteElement.textContent = note.title
+        } else {
+            noteElement.textContent = 'Unnamed note'
+        }
         document.querySelector('#notes').appendChild(noteElement)
     })
 }
@@ -30,40 +35,24 @@ const renderNotes = function (notes, filters) {
 renderNotes(notes, filters)
 
 
-// document.querySelector('#create-note').addEventListener('click', function(e){
-//     console.log('Did this work?')
-//     e.target.textContent = "Button was clicked."
-// })
-
-// document.querySelector('#remove-notes').addEventListener('click', function(e){
-//     document.querySelectorAll('.note').forEach(function(note){
-//         note.remove();
-//     })
-// })
+document.querySelector('#create-note').addEventListener('click', function(e){
+   notes.push({
+       title: '',
+       body: ''
+   })
+   localStorage.setItem('notes', JSON.stringify(notes))
+   renderNotes(notes, filters)
+})
 
 document.querySelector('#search-text').addEventListener('input', function(e){
     filters.searchText = e.target.value;
     renderNotes(notes, filters);
 })
 
-
-// Grab data from NAME FORM
-document.querySelector('#name-form').addEventListener('submit', function(e){
-    // Cancel default behavior of refreshing page
-    e.preventDefault();
-    // elements property provides access to all form fields
-    console.log(e.target.elements.firstName.value);
-    // Clear field after grabbing data
-    e.target.elements.firstName.value = '';
+//Select - drop down filter menu 
+document.querySelector('#filter-by').addEventListener('change', function(e){
+    console.log(e.target.value)
 })
-
-
-// Checkbox
-document.querySelector('#check-box').addEventListener('change', function(e){
-    console.log(e.target.checked)
-})
-
-
 
 
 
