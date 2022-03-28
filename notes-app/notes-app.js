ddef3834cc517f8153e34c5cb5cba69215d01b5b
johns-santos <johnsanthony.santos@gmail.com>
2022-03-28 16:21:52 -0500

    refactoring code

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,65 +1,53 @@
-let notes = []
+const notes = getSavedNotes()
 
+// Search filters object
 const filters = {
-    searchText: ''
+  searchText: ''
 }
 
-// Check for existing saved data
-const notesJSON = localStorage.getItem('notes')
-
-if(notesJSON !== null) {
-    notes = JSON.parse(notesJSON)
-}
-
-
-
-
+// ###################################
+// ===========  START FILTER & RENDER LOGIC =======================================
+// Use filter to return text match between filters and note array
 const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function(note){
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
-    })
-    // CLEAR id=notes div after every input
-    document.querySelector('#notes').innerHTML = '';
-    // Wrap note in paragraph element 
-    filteredNotes.forEach(function (note){
-        const noteElement = document.createElement('p')
-        if(note.title.length > 0) {
-            noteElement.textContent = note.title
-        } else {
-            noteElement.textContent = 'Unnamed note'
-        }
-        document.querySelector('#notes').appendChild(noteElement)
-    })
-}
+  const filteredNotes = notes.filter(function (note) {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+  })
 
+  // Clear #notes div
+  document.querySelector('#notes').innerHTML = ''
+
+  // Iterate over notes array and create 'p' element forEach
+  filteredNotes.forEach(function (note) {
+    let noteEl = document.createElement('p')
+
+    if (noteEl.title.length > 0) {
+      noteEl.textContent = note.title
+    } else {
+      noteEl.textContent = 'Unnamed note'
+    }
+    document.querySelector('#notes').appendChild(noteEl)
+  })
+}// ====  END FILTER  & RENDER LOGIC ===============================
+
+// Execute renderNotes function
 renderNotes(notes, filters)
 
-
-document.querySelector('#create-note').addEventListener('click', function(e){
-   notes.push({
-       title: '',
-       body: ''
-   })
-   localStorage.setItem('notes', JSON.stringify(notes))
-   renderNotes(notes, filters)
+document.querySelector('#add-note').addEventListener('click', function (e) {
+  notes.push({
+    title: '',
+    body: ''
+  })
+  localStorage.setItem('notes', JSON.stringify(notes))
+  renderNotes(notes, filters)
 })
 
-document.querySelector('#search-text').addEventListener('input', function(e){
-    filters.searchText = e.target.value;
-    renderNotes(notes, filters);
+// Search text input box (use input for realtime search) - Populates 'filters' array and uses filter to return text match between notes and filters array
+document.querySelector('#search-text').addEventListener('input', function (e) {
+  filters.searchText = e.target.value
+  renderNotes(notes, filters)
 })
 
-//Select - drop down filter menu 
-document.querySelector('#filter-by').addEventListener('change', function(e){
-    console.log(e.target.value)
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+  const contentTest = e.target.value
+  console.log(contentTest)
 })
-
-
-
-
-
-
-
-
-
-
