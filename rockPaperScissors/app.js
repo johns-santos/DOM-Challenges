7d3fff67b50d114e25e602bf0886db327a8eb559
johns-santos <johnsanthony.santos@gmail.com>
2022-04-02 01:07:45 -0500

    Adding rock, paper, scissors project

---
+++ rockPaperScissors/app.js
@@ -0,0 +1,57 @@
+const startGameBtn = document.getElementById('start-game-btn')
+
+const ROCK = 'ROCK'
+const PAPER = 'PAPER'
+const SCISSORS = 'SCISSORS'
+const DEFAULT_USER_CHOICE = ROCK
+const RESULT_DRAW = 'DRAW'
+const RESULT_PLAYER_WINS = 'PLAYER_WINS'
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS'
+
+let gameIsRunning = false
+
+const getPlayerChoice =  () => {
+  const selection = prompt(
+    `${ROCK}, ${PAPER}, or ${SCISSORS}?`,
+    ''
+  ).toUpperCase()
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`)
+    return DEFAULT_USER_CHOICE
+  }
+  return selection
+};
+
+const getComputerChoice = () => {
+  const randomValue = Math.random()
+  if (randomValue < 0.34) {
+    return ROCK
+  } else if (randomValue < 0.67) {
+    return PAPER
+  } else {
+    return SCISSORS
+  }
+}
+
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS
+
+startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return
+  }
+  gameIsRunning = true
+  console.log('Game has started......')
+  const playerChoice = getPlayerChoice()
+  const computerChoice = getComputerChoice()
+  const winner = getWinner(computerChoice, playerChoice)
+
+  alert(winner)
+  gameIsRunning = false
+})
--- unconventionalCalculator/assets/scripts/app.js
+++ unconventionalCalculator/assets/scripts/app.js
@@ -30,46 +30,57 @@ function writeToLog (
   console.log(logEntries)
 }
 
-function calculateResult(calculationType) {
+
+function calculateResult (calculationType) {
   const enteredNumber = getUserNumberInput()
-  const initialResult = currentResult;
-  let mathOperator;
-  if(calculationType === 'ADD'){
-    currentResult += enteredNumber;
-    mathOperator = '+';
-  } 
-  else if(calculationType === 'MULTIPLY'){
-    currentResult *= enteredNumber;
-    mathOperator = '*';
-  }
-  else if(calculationType === 'DIVIDE'){
-    currentResult /= enteredNumber;
-    mathOperator = '/';
-  }
-  else if (calculationType === 'SUBTRACT') {
-    currentResult -= enteredNumber;
-    mathOperator = '-';
-  }
-  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+
+// If none of the cases are meet then do run rest of code.
+if (
+  calculationType !== 'ADD' &&
+  calculationType !== 'SUBTRACT' &&
+  calculationType !== 'MULTIPLY' &&
+  calculationType !== 'DIVIDE' ||
+  // If enteredNumber is falsie (anything other than 0)
+  !enteredNumber
+) {
+  return
 }
 
 
+  const initialResult = currentResult
+  let mathOperator
+  if (calculationType === 'ADD') {
+    currentResult += enteredNumber
+    mathOperator = '+'
+  } else if (calculationType === 'MULTIPLY') {
+    currentResult *= enteredNumber
+    mathOperator = '*'
+  } else if (calculationType === 'DIVIDE') {
+    currentResult /= enteredNumber
+    mathOperator = '/'
+  } else if (calculationType === 'SUBTRACT') {
+    currentResult -= enteredNumber
+    mathOperator = '-'
+  }
+
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber)
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult)
+}
+
 function add () {
-  calculateResult('ADD');
-  
+  calculateResult('ADD')
 }
 
 function subtract () {
-  calculateResult('SUBTRACT');
+  calculateResult('SUBTRACT')
 }
 
 function multiply () {
-  calculateResult('MULTIPLY');
+  calculateResult('MULTIPLY')
 }
 
 function divide () {
-  calculateResult('DIVIDE');
+  calculateResult('DIVIDE')
 }
 
 addBtn.addEventListener('click', add)
