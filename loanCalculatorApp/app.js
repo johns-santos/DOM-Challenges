42c118a0e49e21bf35dfe92283e763cf2670a81c
johns-santos <johnsanthony.santos@gmail.com>
2022-04-20 19:08:57 -0500

    added calculator logic & error messsage

--- loanCalculatorApp/app.js
+++ loanCalculatorApp/app.js
@@ -1,1 +1,54 @@
-alert(1)
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+
+function calculateResults(e) {
+   
+    // UI variables
+    const UIamount = document.getElementById('amount');
+    const UIinterest = document.getElementById('interest');
+    const UIyears  = document.getElementById('years');
+    const UImonthlyPayment      = document.getElementById('monthly-payment');
+    const UItotalPayment = document.getElementById('total-payment');
+    const UItotalInterest = document.getElementById('total-interest');
+
+    // Value variables to be used in calculation
+    const principal = parseFloat(UIamount.value);
+    const calculatedIntereest = parseFloat(UIinterest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(UIyears.value) * 12;
+
+    // Calculate monthly payments
+    const x = Math.pow(1 + calculatedIntereest, calculatedPayments);
+    const monthly = (principal * x * calculatedIntereest) / (x - 1);
+
+    // Determine if finite number ( Validation)
+    if(isFinite(monthly)) {
+        UImonthlyPayment.value = monthly.toFixed(2);
+        UItotalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        UItotalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+           // Create error ------ 
+           showError('Please check your numbers.');
+    }
+
+    e.preventDefault();
+
+}
+
+// Show Error Function - using createElement
+function showError(error){
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+    
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading - usng insertBefore DOM method and class names
+    card.insertBefore(errorDiv, heading);
+}
