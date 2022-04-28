596da1801a3f42ab0a5f9ec4df80647b176c47c5
johns-santos <johnsanthony.santos@gmail.com>
2022-04-27 21:04:01 -0500

    refactoring - created a gameOver function

--- numberGuesserApp/app.js
+++ numberGuesserApp/app.js
@@ -28,48 +28,50 @@ UIMaxNum.textContent = max
 UIGuessBtn.addEventListener('click', function () {
   let guess = parseInt(UIGuessInput.value)
   console.log(guess)
+
   // validate
   if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}.`, 'red')
-    UIGuessInput.style.borderColor = 'red'
   }
 
-  // CHeck if WON
+  // Check if WON
   if (guess === winningNum) {
-    // Disable input
-    UIGuessInput.disabled = true
-    // Change border color
-    UIGuessInput.style.borderColor = 'green'
-    // Set message
-    setMessage(`${winningNum} is correct!`, 'green')
+    gameOver(true, `${winningNum} is correct!`)
+
   } else {
     // Wrong guess ***********************
-    guessesLeft -= 1;
+    guessesLeft -= 1
     setMessage(`You have ${guessesLeft} left.`)
 
     if (guessesLeft === 0) {
       // GAME OVER - lost
-
-      // Disable input
-      UIGuessInput.disabled = true;
-      UIGuessBtn.disabled = true;
-      // Change border color
-      UIGuessInput.style.borderColor = 'red'
-      // Set message
-      setMessage(`Game Over. You Lose!`, 'red')
+      gameOver(false, `Game Over. You Lose!`)
     } else {
       // Game continues - answer wrong *****************
-        UIGuessInput.style.borderColor = 'red';
+      UIGuessInput.style.borderColor = 'red'
 
-        // Clear input
-        UIGuessInput.value = '';
+      // Clear input
+      UIGuessInput.value = ''
 
-        // Notify user guess is not correct.
-        setMessage(`(${guess}) is incorrect. ${guessesLeft} guesses remaining.`)
-
+      // Notify user guess is not correct.
+      setMessage(`(${guess}) is incorrect. ${guessesLeft} guesses remaining.`)
     }
   }
 })
+
+// GAME OVER function
+function gameOver (won, msg) {
+  let color
+  won === true ? (color = 'green') : (color = 'red')
+  // Disable input
+  UIGuessInput.disabled = true
+  // Change border color
+  UIGuessInput.style.borderColor = color
+  // Set text color
+  UIMessage.style.color = color;
+  // Set message
+  setMessage(msg)
+}
 
 // Set message - use color as a second property.
 function setMessage (msg, color) {
